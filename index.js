101e6e0232c7d953260cf484c5d464dbf340c3f0
Roxanne <roxanne.c.alezine@gmail.com>
2024-11-05 21:24:16 +0100

    using server request

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
-const sampleEmployee = {
-  name: {
-    first: "Charlie",
-    last: "Thompson",
-  },
-  email: "charlie.thompson@example.com",
-  picture: {
-    medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
-  },
-};
+app.use(cors({ origin: ["http://localhost:3000"] }));
 
-app.get("/api/employees", (req, res) => {
-  res.json({ results: [sampleEmployee] });
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const port = 3310;
